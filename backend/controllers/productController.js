b87f071cde1cd71869dce9fa65e4246b39175f84
vivek13420 <vivekrana13420@gmail.com>
2023-01-18 22:04:31 +0530

    first commit

---
+++ backend/controllers/productController.js
@@ -0,0 +1,3 @@
+exports.getAllProducts = (req, res) => {
+  res.status(200).json({ msg: "Route is working fine" });
+};
---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const app = require("./app");
+const dotenv = require("dotenv");
+const connectDatabase = require("./config/database");
+
+//Config
+dotenv.config({ path: "backend/config/config.env" });
+
+//Connecting to database
+connectDatabase()
+
+app.listen(process.env.PORT, () => {
+  console.log("listening on port " + process.env.PORT);
+});
